c05e909ecdf6567da9c6ade46297d36e7f2864ed
albert-sobreo <janalbertsobreo@gmail.com>
2025-08-27 15:51:59 +0800

    first commit

---
+++ src/components/TypingEffect.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import {ref, defineProps, computed} from 'vue'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+    default: '',
+  }
+})
+
+</script>
