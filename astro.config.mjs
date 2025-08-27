35f065a42bef71a29273ab3bef3c42de3cc3ea8f
albert-sobreo <janalbertsobreo@gmail.com>
2025-08-27 16:08:32 +0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
+  base: '/',
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
